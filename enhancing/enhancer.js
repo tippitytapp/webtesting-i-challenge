f8e628f10cf292c4d15119550886bb5fe52727b3
Marc Tapp <marc.tapp@gmail.com>
2020-05-20 17:27:33 -0400

    finished MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,29 +7,50 @@ module.exports = {
 
 const item = {
   name: "marc",
-  durability: 10,
+  durability: 100,
   enhancement: 20
 }
 
 function succeed(item) {
-  console.log('item.enh', item.enhancement)
   if(item.enhancement < 20){
     let enhance = ++item.enhancement
     return{...item, enhancement: enhance}
   } else if(item.enhancement == 20){
     return item;
-  } else if(item.enhancement > 20) 
-    throw 'enhancement should be no more than 20';  
-}
-console.log("succeed", succeed(item));
-function fail(item) {
-  return { ...item };
+  } else{
+    let enhance = 20
+    return{...item, enhancement: enhance}
+  }
 }
 
-function repair(item) {
-  return { ...item, durability: 100 };
+function fail(item) {
+  if(item.enhancement < 15){
+    let durab = item.durability - 5;
+    console.log('durab fail', durab)
+    return {...item, durability: durab}
+  } else if (item.enhancement === 15){
+    let durab = item.durability - 10;
+    console.log('durab fail', durab)
+    return {...item, durability: durab}
+  } else if (item.enhancement >= 16){
+    let enhance = item.enhancement - 1;
+    let durab = item.durability - 10;
+    console.log(enhance, durab)
+    return {...item, enhancement: enhance, durability: durab}
+  } else {
+    return item;
+  }
 }
-console.log("repair", repair(item));
+fail(item);
+function repair(item) {
+  if(item.durability < 100){
+  return { ...item, durability: 100 };
+} else {
+  let dura = 100
+  return {...item, durability: dura}
+}
+}
+
 function get(item) {
   return { ...item };
 }
