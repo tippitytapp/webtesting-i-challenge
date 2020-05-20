dc167ad098428a41a98b8351c351384272366b97
Marc Tapp <marc.tapp@gmail.com>
2020-05-20 15:55:11 -0400

    got the succedd and the repair functions working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,31 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
-  return { ...item };
+const item = {
+  name: "marc",
+  durability: 10,
+  enhancement: 20
 }
 
+function succeed(item) {
+  console.log('item.enh', item.enhancement)
+  if(item.enhancement < 20){
+    let enhance = ++item.enhancement
+    return{...item, enhancement: enhance}
+  } else if(item.enhancement == 20){
+    return item;
+  } else if(item.enhancement > 20) 
+    throw 'enhancement should be no more than 20';  
+}
+console.log("succeed", succeed(item));
 function fail(item) {
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
-
+console.log("repair", repair(item));
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,2 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
